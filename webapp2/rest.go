b2b152823a1e8f6ea3c82bb639547123f1936696
juragan360 <ariefda@hotmail.com>
2016-11-29 20:32:40 +0700

    update

--- webapp2/rest.go
+++ webapp2/rest.go
@@ -28,3 +28,34 @@ func (r *Rest) Tables(ctx *knot.WebContext) interface{} {
 
 	return toolkit.NewResult().SetData(data)
 }
+
+type DataRequest struct {
+	Table string
+	Take  int
+}
+
+var e error
+
+func (r *Rest) Data(ctx *knot.WebContext) interface{} {
+	ctx.Config.OutputType = knot.OutputJson
+	res := toolkit.NewResult()
+
+	datareq := new(DataRequest)
+	if e = ctx.GetPayload(&datareq); e != nil {
+		return res.SetErrorTxt("Unable to load request")
+	}
+
+	c := conn()
+	defer c.Close()
+
+	cs, ecs := c.NewQuery().From(datareq.Table).Take(datareq.Take).Select().Cursor(nil)
+	if ecs != nil {
+		return res.SetErrorTxt("Error preparing query: " + ecs.Error())
+	}
+	defer cs.Close()
+
+	data := []*toolkit.M{}
+	cs.Fetch(&data, 0, false)
+
+	return res.SetData(data)
+}
